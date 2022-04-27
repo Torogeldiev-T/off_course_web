dcac7511753a2784343e6cf2e4f159738137fab3
trekvartista <isken_150502@mail.ru>
2022-04-27 18:16:34 +0600

    profile info needs a fix

--- src/components/Profile/Info.jsx
+++ src/components/Profile/Info.jsx
@@ -3,21 +3,25 @@ import React from "react";
 const Info = ({ profile }) => {
     return (
         <div className="flex flex-row">
-
             <div className="flex flex-col w-1/2">
                 {Object.keys(profile).map(
                     (key, index) =>
                         !Array.isArray(profile[key]) && (
                             <div
                                 key={index}
-                                className="w-full border-b-[3px] p-3 "
+                                className="w-full border-b-[3px] p-3 relative"
                             >
-                                <b>{key}</b>
+                                <b className="align-middle ">{key}</b>
+                                <div className="inline absolute left-32 w-full">
+                                    <span className="align-middle">
+                                        {profile[key]}
+                                    </span>
+                                </div>
                             </div>
                         )
                 )}
             </div>
-            <div className="flex flex-col w-full ">
+            {/* <div className="flex flex-col w-full ">
                 {Object.keys(profile).map(
                     (key, index) =>
                         !Array.isArray(profile[key]) && (
@@ -29,7 +33,7 @@ const Info = ({ profile }) => {
                             </div>
                         )
                 )}
-            </div>
+            </div> */}
         </div>
     );
 };
